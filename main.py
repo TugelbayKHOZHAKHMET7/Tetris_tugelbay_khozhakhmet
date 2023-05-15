f145124de3189dc7647947a5665837e8634fd8ea
TugelbayKHOZHAKHMET7 <133655840+TugelbayKHOZHAKHMET7@users.noreply.github.com>
2023-05-15 16:59:19 +0500

    main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pygame
 from copy import deepcopy
 from random import choice, randrange
 
-width, hight = 10, 20 #W,H
-rec_scale = 35  #TILE
-game_cons = width * rec_scale, hight * rec_scale #GAME_RES
-whole_cons = 750, 795 #RES
+width, hight = 10, 20
+rec_scale = 35  
+game_cons = width * rec_scale, hight * rec_scale 
+whole_cons = 750, 795 
 FPS = 60
 
 pygame.init()
-cons_screen = pygame.display.set_mode(whole_cons) #sc
-game_screen = pygame.Surface(game_cons) #game_sc
+cons_screen = pygame.display.set_mode(whole_cons) 
+game_screen = pygame.Surface(game_cons) 
 clock = pygame.time.Clock()
 
 grid = [pygame.Rect(x * rec_scale, y * rec_scale, rec_scale, rec_scale) for x in range(width) for y in range(hight)]
@@ -29,15 +29,15 @@ field = [[0 for i in range(width)] for j in range(hight)]
 
 anim_count, anim_speed, anim_limit = 0, 60, 2000
 
-cons_picture = pygame.image.load('env_phon.jpg').convert() #bg
-game_cons_picture = pygame.image.load('priroda.jpg').convert() #game_bg
+cons_picture = pygame.image.load('env_phon.jpg').convert() 
+game_cons_picture = pygame.image.load('priroda.jpg').convert() 
 
-main_type = pygame.font.Font('FiraMono-Medium.ttf', 65) #main_font
-font = pygame.font.Font('FiraMono-Medium.ttf', 45) #font
+main_type = pygame.font.Font('FiraMono-Medium.ttf', 65) 
+font = pygame.font.Font('FiraMono-Medium.ttf', 45) 
 
 title_tetris = main_type.render('TETRIS', True, pygame.Color('darkorange'))
-pl_score = font.render('баллы:', True, pygame.Color('green')) #title_score
-pl_record = font.render('рекорд:', True, pygame.Color('purple')) # title_record
+pl_score = font.render('баллы:', True, pygame.Color('green')) 
+pl_record = font.render('рекорд:', True, pygame.Color('purple'))
 
 make_color = lambda : (randrange(30, 256), randrange(30, 256), randrange(30, 256))
 
@@ -77,10 +77,9 @@ while True:
     cons_screen.blit(cons_picture, (0, 0))
     cons_screen.blit(game_screen, (20, 20))
     game_screen.blit(game_cons_picture, (0, 0))
-    # delay for full lines
     for i in range(lines):
         pygame.time.wait(200)
-    # control
+   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
@@ -93,14 +92,13 @@ while True:
                 anim_limit = 100
             elif event.key == pygame.K_UP:
                 rotate = True
-    # move x
+    
     piece_old = deepcopy(piece)
     for i in range(4):
         piece[i].x += dx
         if not check_borders():
             piece = deepcopy(piece_old)
             break
-    # move y
     anim_count += anim_speed
     if anim_count > anim_limit:
         anim_count = 0
@@ -114,7 +112,7 @@ while True:
                 next_piece, next_color = deepcopy(choice(figures)), make_color()
                 anim_limit = 2000
                 break
-    # rotate
+   
     center = piece[0]
     piece_old = deepcopy(piece)
     if rotate:
@@ -126,7 +124,6 @@ while True:
             if not check_borders():
                 figure = deepcopy(piece_old)
                 break
-    # check lines
     line, lines = hight - 1, 0
     for row in range(hight - 1, -1, -1):
         count = 0
@@ -139,33 +136,29 @@ while True:
         else:
             anim_speed += 3
             lines += 1
-    # compute score
+    
     score += scores[lines]
-    # draw grid
     [pygame.draw.rect(game_screen, (40, 40, 40), i_rect, 1) for i_rect in grid]
-    # draw figure
     for i in range(4):
         figure_rect.x = piece[i].x * rec_scale
         figure_rect.y = piece[i].y * rec_scale
         pygame.draw.rect(game_screen, color, figure_rect)
-    # draw field
     for y, raw in enumerate(field):
         for x, col in enumerate(raw):
             if col:
                 figure_rect.x, figure_rect.y = x * rec_scale, y * rec_scale
                 pygame.draw.rect(game_screen, col, figure_rect)
-    # draw next figure
     for i in range(4):
         figure_rect.x = next_piece[i].x * rec_scale + 380
         figure_rect.y = next_piece[i].y * rec_scale + 185
         pygame.draw.rect(cons_screen, next_color, figure_rect)
-    # draw titles
+   
     cons_screen.blit(title_tetris, (485, -10))
     cons_screen.blit(pl_score, (535, 550)) #535,780
     cons_screen.blit(font.render(str(score), True, pygame.Color('black')), (550, 600))
     cons_screen.blit(pl_record, (525, 650))
     cons_screen.blit(font.render(record, True, pygame.Color('gold')), (550, 710))
-    # game over
+    
     for i in range(width):
         if field[0][i]:
             set_record(record, score)
